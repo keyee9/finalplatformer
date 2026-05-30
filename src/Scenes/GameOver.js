a37159933a1709d7f67a281f49544f13ed38e6cd
keyee9 <kegyee@ucsc.edu>
2026-05-29 20:43:21 -0700

    edea

---
+++ src/Scenes/GameOver.js
@@ -0,0 +1,62 @@
+class GameOver extends Phaser.Scene {
+    constructor() {
+        super("gameOver");
+        this.highscores = [];
+    }
+
+    init(data) {
+        this.finalScore = data.score || 0;
+
+
+    }
+    
+    preload() {
+        // Preload assets here if needed
+    }
+
+    create() {
+        // Clear background color fallback to black
+        this.cameras.main.setBackgroundColor('#000000');
+
+        // FIXED: Use height for the Y coordinate centering!
+        let centerX = this.sys.game.config.width / 2;
+        let centerY = this.sys.game.config.height / 2;
+
+        // 1. GAME OVER HEADER TEXT
+        this.add.text(centerX-120, centerY+300 , "DEAD", {
+            fontSize: "100px",
+            color: "#ff0000",
+            stroke: "#000000",
+            strokeThickness: 20
+        }).setOrigin(0, 5); // Centers the text block perfectly
+
+        // 2. COINS GATHERED TEXT
+        // FIXED: Corrected string concatenation syntax
+        this.coinText = this.add.text(centerX, centerY, "Coins gathered: " + this.finalScore, {
+            fontSize: "75px",
+            color: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 20
+        }).setOrigin(0.5);
+
+
+        // 5. RESTART INSTRUCTION
+        this.add.text(centerX, centerY + 140, "R to return", {
+            fontSize: "100px",
+            color: "#aaaaaa"
+        }).setOrigin(0.5);
+
+        // Listen for the R key to restart the game
+        this.rKey = this.input.keyboard.addKey('R');
+    }
+    
+
+    update(time, delta) {
+        // If player presses R, kick them back to your main level scene
+        if (Phaser.Input.Keyboard.JustDown(this.rKey)) {
+                        this.sound.play("revivesfx");
+
+            this.scene.start("platformerScene");
+        }
+    }
+}
--- src/Scenes/Platformer.js
+++ src/Scenes/Platformer.js
@@ -5,18 +5,31 @@ class Platformer extends Phaser.Scene {
 
     init() {
         // variables and settings
-        this.ACCELERATION = 400;
+        this.ACCELERATION = 150;
         this.DRAG = 500;    // DRAG < ACCELERATION = icy slide
-        this.physics.world.gravity.y = 1500;
+        this.physics.world.gravity.y = 1800;
         this.JUMP_VELOCITY = -600;
-        this.PARTICLE_VELOCITY = 50;
+        this.PARTICLE_VELOCITY = 0;
         this.SCALE = 2.0;
+        this.firstlevel=true;
+        
     }
 
     create() {
-        // Create a new tilemap game object which uses 18x18 pixel tiles, and is
-        // 45 tiles wide and 25 tiles tall.
-        this.physics.world.setBounds(0,0,2700,550);
+        this.jumpCount = 0;
+        this.maxJumps = 0;  
+        this.jumpsLeft = 0;
+        this.coinCount = 0;
+                
+        this.coinText = this.add.text(780, 20, "Coins: 0", {
+            fontSize: "24px",
+            color: "#ffffff",
+            stroke: "#000000",
+            strokeThickness: 4
+            
+        });
+        // Create
+
         this.map = this.add.tilemap("platformer-level-1", 18, 18, 150, 25);
 
         // Add a tileset to the map
@@ -24,11 +37,14 @@ class Platformer extends Phaser.Scene {
         // Second parameter: key for the tilesheet (from this.load.image in Load.js)
         this.tileset = this.map.addTilesetImage("kenny_tilemap_packed", "tilemap_tiles");
         this.bgset= this.map.addTilesetImage("kenny_tilemap_packed_air", "bgmap_tiles");
-        
+        this.physics.world.setBounds(0,0,900,550);
+
         // Create a layer
+        this.deathlayer = this.map.createLayer("Death", this.tileset, 0,0);
         this.groundLayer = this.map.createLayer("Ground-n-Platforms", this.tileset, 0, 0);
         this.bgLayer=this.map.createLayer("Background", this.bgset, 0, 0);
         this.bgLayer.setDepth(-1);
+        this.doorlayer=this.map.createLayer("Door", this.tileset, 0,0);
         // Make it collidable
         this.groundLayer.setDepth(0);
         this.groundLayer.setCollisionByProperty({
@@ -61,12 +77,15 @@ class Platformer extends Phaser.Scene {
 
         // Enable collision handling
         this.physics.add.collider(my.sprite.player, this.groundLayer);
-
+        
         // TODO: Add coin collision handler
         
         // Handle collision detection with coins
         this.physics.add.overlap(my.sprite.player, this.coinGroup, (obj1, obj2) => {
             obj2.destroy(); // remove coin on overlap
+            this.coinCount++;
+            this.sound.play("coinsfx");
+this.coinText.setText("Coins: " + this.coinCount);
         });
         // set up Phaser-provided cursor key input
         cursors = this.input.keyboard.createCursorKeys();
@@ -80,18 +99,28 @@ class Platformer extends Phaser.Scene {
         }, this);
 
         // TODO: Add movement vfx here
-                my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
+        
+        my.vfx.walking = this.add.particles(0, 0, "kenny-particles", {
             frame: ['smoke_03.png', 'smoke_09.png'],
             // TODO: Try: add random: true
             scale: {start: 0.03, end: 0.1},
             // TODO: Try: maxAliveParticles: 8,
-            lifespan: 300,
+            
+            lifespan: 350,
             // TODO: Try: gravityY: -400,
             alpha: {start: 1, end: 0.1}, 
         });
 
         my.vfx.walking.stop();
 
+        
+        // my.vfx.jumping = this.add.particles(0,0, "kenny-particles",{
+        //   frame:['muzzle_01.png','muzzle_05.png'],
+        //   scale:{scale: 0.03, end:0.1},
+        //   lifespan:350,
+        //  alpha: {start:1, end:0.1},
+        //        });
+        //   my.vfx.jumping.stop();
 
         // TODO: add camera code here
         
@@ -99,6 +128,39 @@ class Platformer extends Phaser.Scene {
         this.cameras.main.startFollow(my.sprite.player, true, 0.25, 0.25); // (target, [,roundPixels][,lerpX][,lerpY])
         this.cameras.main.setDeadzone(50, 50);
         this.cameras.main.setZoom(this.SCALE);
+        this.coinText.setScrollFactor(0,1);
+        this.coinText.setOrigin(-1.25,0);
+
+
+        this.deathlayer.setCollisionByProperty({
+            dead: true
+        })
+        
+        this.physics.add.collider(my.sprite.player,this.deathlayer, () => {
+this.sound.play('deathsfx', {
+   volume: 0.5 
+
+
+});
+this.bgMusic.stop();
+
+this.scene.start("gameOver", { score: this.coinCount    });  
+        
+        });
+
+this.bgMusic  = this.sound.add('bgm',  { volume: 0.3, loop: true });
+this.bgMusic.play();
+
+this.doorlayer.setCollisionByProperty({
+    win:true
+})
+
+
+
+
+ 
+
+
     }
 
     update() {
@@ -129,6 +191,8 @@ class Platformer extends Phaser.Scene {
 
             // Only play smoke effect if touching the ground
 
+            if (my.sprite.ACCELERATION>150) my.sprite.ACCELERATION=150;
+
             if (my.sprite.player.body.blocked.down) {
 
                 my.vfx.walking.start();
@@ -146,17 +210,53 @@ class Platformer extends Phaser.Scene {
             // TODO: have the vfx stop playing
         }
 
+        if (this.coinCount >=5 && this.firstlevel) 
+                this.physics.world.setBounds(0,0,1800,550);
+
+        if (this.coinCount >=10){
+            this.firstlevel=false;
+            this.physics.world.setBounds(0,0,2700,550);
+
+        }
+        
+
+
+
         // player jump
         // note that we need body.blocked rather than body.touching b/c the former applies to tilemap tiles and the latter to the "ground"
-        if(!my.sprite.player.body.blocked.down) {
-            my.sprite.player.anims.play('jump');
+if (my.sprite.player.body.blocked.down) {
+            this.jumpsLeft = 2;
         }
-        if(my.sprite.player.body.blocked.down && Phaser.Input.Keyboard.JustDown(cursors.up)) {
-            my.sprite.player.body.setVelocityY(this.JUMP_VELOCITY);
+        if (!my.sprite.player.body.blocked.down) {
+            my.sprite.player.anims.play('jump', true);
         }
 
-        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
-            this.scene.restart();
+        if (Phaser.Input.Keyboard.JustDown(cursors.up) && this.jumpsLeft > 0) {
+            my.sprite.player.setVelocityY(this.JUMP_VELOCITY);
+this.sound.play('jumpsfx', {
+   volume: 0.5 
+
+
+    });
+            this.jumpsLeft--;
         }
+        if(Phaser.Input.Keyboard.JustDown(this.rKey)) {
+            this.bgMusic.stop();
+
+        this.scene.start("winScreen", { score: this.coinCount    });  
+            }
+
+
+if (this.coinCount>=15){
+    this.physics.add.collider(my.sprite.player, this.doorlayer, () =>{
+       this.scene.start("winScreen", { score: this.coinCount    }); 
+
+    });
+}
+
+    
+
+
+
     }
 }
